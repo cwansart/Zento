b3c0e3f0c4827408c6c10699430c6fe98ded3733
Christian Wansart <christian@bluepanel.org>
2015-07-14 16:27:30 +0200

    added ExamResult model and migration

---
+++ database/migrations/2015_07_14_142608_create_exam_results_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExamResultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exam_results', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('exam_id');
+            $table->integer('user_id');
+            $table->string('result');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('exam_results');
+    }
+}

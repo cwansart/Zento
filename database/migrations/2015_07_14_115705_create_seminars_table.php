29822b9aa87166adefa0eeee82dd0eee3786d142
Christian Wansart <christian@bluepanel.org>
2015-07-14 13:59:35 +0200

    added Seminar model and migration

---
+++ database/migrations/2015_07_14_115705_create_seminars_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSeminarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seminars', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('location_id');
+            $table->string('title');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('seminars');
+    }
+}

acc54d730593c983186f26dadf175a0bb2399b16
Christian Wansart <christian@bluepanel.org>
2015-07-25 22:11:32 +0200

    added store action to ExamController

--- app/Exam.php
+++ app/Exam.php
@@ -13,6 +13,19 @@ class Exam extends Model
      */
     protected $dates = ['date'];
 
+    /**
+     * Sets the validator rules.
+     *
+     * @var array
+     */
+    static public $rules = [
+        'street' => 'required|min:2|required', // we can't use "alpha" here since some streets are abbreviated with a dot
+        'housenr' => 'required|min:2|alphanum|required', // since we save it as a string for occasions like "12b" alphanum's fine
+        'zip' => 'digits:5|required', // are there any zipcodes that have more than 5 digits?
+        'city' => 'min:2|alpha|required',
+        'date' => 'date|required',
+    ];
+
     public function location()
     {
         return $this->belongsTo('Zento\Location');
--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -8,7 +8,9 @@ use Zento\ExamResult;
 use Zento\Http\Requests;
 use Zento\Http\Controllers\Controller;
 
+use Zento\Location;
 use Zento\Exam;
+use Validator;
 
 class ExamController extends Controller
 {
@@ -38,9 +40,47 @@ class ExamController extends Controller
      *
      * @return Response
      */
-    public function store()
+    public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), Exam::$rules);
+        //dd($request);
+
+        if ($validator->fails()) {
+            return redirect(action('ExamController@index'))
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        // TODO: extract this block to the Location model
+        // check if given location exists; use or create otherwise.
+        $location = Location::where('street', '=', $request->input('street'))
+            ->where('housenr', '=', $request->input('housenr'))
+            ->where('zip', '=', $request->input('zip'))
+            ->where('city', '=', $request->input('city'))
+            ->where('country', '=', $request->input('country'));
+        if(!$location->exists()) {
+            // create location if it doesn't exist
+            $location = Location::create([
+                // do we really need a name in a location? We'll leave it blank for now...
+                //'name' => $request->input('firstname').' '.$request->input('lastname'),
+                'street' => $request->input('street'),
+                'housenr' => $request->input('housenr'),
+                'zip' => $request->input('zip'),
+                'city' => $request->input('city'),
+                'country' => $request->input('country')
+            ]);
+        } else {
+            // we need to call get() otherwise we'd have an query object
+            $location = $location->first();
+        }
+
+        $exam = new Exam();
+        $exam->date = \Carbon\Carbon::createFromFormat('d.m.Y', $request->input('date')); // TODO: We should find a more localized friendly version; this way only the German date format works.
+        $exam->location_id = $location->id;
+        $exam->save();
+
+        // TODO: add success information to view
+        return redirect(action('ExamController@index'));
     }
 
     /**

90c436a8ecae8863418cf5122f514b3533d2e3b5
Christian Wansart <christian@bluepanel.org>
2015-07-26 22:16:17 +0200

    removed unnecessary 'required' from rules

--- app/Exam.php
+++ app/Exam.php
@@ -19,8 +19,8 @@ class Exam extends Model
      * @var array
      */
     static public $rules = [
-        'street' => 'required|min:2|required', // we can't use "alpha" here since some streets are abbreviated with a dot
-        'housenr' => 'required|min:2|alphanum|required', // since we save it as a string for occasions like "12b" alphanum's fine
+        'street' => 'required|min:2', // we can't use "alpha" here since some streets are abbreviated with a dot
+        'housenr' => 'required|min:2|alphanum', // since we save it as a string for occasions like "12b" alphanum's fine
         'zip' => 'digits:5|required', // are there any zipcodes that have more than 5 digits?
         'city' => 'min:2|alpha|required',
         'date' => 'date|required',

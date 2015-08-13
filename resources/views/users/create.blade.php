2f3995aab5d6636e92e62331208d8e3bfb6f6bf0
Christian Wansart <christian@bluepanel.org>
2015-08-13 22:46:10 +0200

    moved users create action to modal dialog #15

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,101 +1,118 @@
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Benutzer erstellen</div>
-                <div class="panel-body">
-                    @if (count($errors) > 0)
-                        <div class="alert alert-danger">
-                            Es gab ein paar Probleme.<br><br>
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Benutzer erstellen</button>
 
-                    {!! Form::open(array('class' => 'form-horizontal', 'method' => 'POST', 'route' => 'users.store')) !!}
-                    <div class="form-group">
-                        {!! Form::label('firstname', 'Vorname', ['class' => 'col-md-4 control-label']) !!}
-                        <div class="col-md-6">
-                            {!! Form::input('text', 'firstname', null, ['class' => 'form-control', 'placeholder' => 'Vorname', 'required']) !!}
-                        </div>
-                    </div>
+<div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
 
-                    <div class="form-group">
-                        {!! Form::label('lastname', 'Nachname', ['class' => 'col-md-4 control-label']) !!}
-                        <div class="col-md-6">
-                            {!! Form::input('text', 'lastname', null, ['class' => 'form-control', 'placeholder' => 'Nachname', 'required']) !!}
-                        </div>
-                    </div>
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Benutzer erstellen</h4>
+            </div>
+            <div class="modal-body">
+                <div class="container-fluid">
+                    <div class="row">
+                        @if (count($errors) > 0)
+                            <div class="alert alert-danger">
+                                Es gab ein paar Probleme.<br><br>
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
 
-                    <div class="form-group">
-                        {!! Form::label('email', 'Email-Adresse', ['class' => 'col-md-4 control-label']) !!}
-                        <div class="col-md-6">
-                            {!! Form::input('email', 'email', null, ['class' => 'form-control', 'placeholder' => 'Email-Adresse', 'required']) !!}
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        {!! Form::label('password', 'Passwort', ['class' => 'col-md-4 control-label']) !!}
-                        <div class="col-md-6">
-                            {!! Form::input('password', 'password', null, ['class' => 'form-control', 'placeholder' => 'Leer lassen zum Deaktivieren']) !!}
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        {!! Form::label('birthday', 'Geburtstag', ['class' => 'col-md-4 control-label']) !!}
-                        <div class="col-md-6">
-                            <div class="input-group date datetimepicker" data-date-format="DD.MM.YYYY">
-                                {!! Form::input('text', 'birthday', null, ['class' => 'form-control', 'placeholder' => 'z. B. 01.07.1985']) !!}
-                                <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"</span>
+                        {!! Form::open(array('class' => 'form-horizontal', 'method' => 'POST', 'route' => 'users.store')) !!}
+                        <div class="form-group">
+                            {!! Form::label('firstname', 'Vorname', ['class' => 'col-md-4 control-label']) !!}
+                            <div class="col-md-6">
+                                {!! Form::input('text', 'firstname', null, ['class' => 'form-control', 'placeholder' => 'Vorname', 'required']) !!}
                             </div>
                         </div>
-                    </div>
 
-                    <div class="form-group">
-                        {!! Form::label('entry_date', 'Eintrittsdatum', ['class' => 'col-md-4 control-label']) !!}
-                        <div class="col-md-6">
-                            <div class="input-group date datetimepicker" data-date-format="DD.MM.YYYY">
-                                {!! Form::input('text', 'entry_date', null, ['class' => 'form-control', 'placeholder' => 'z. B. '.date('d.m.Y')]) !!}
-                                <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"</span>
+                        <div class="form-group">
+                            {!! Form::label('lastname', 'Nachname', ['class' => 'col-md-4 control-label']) !!}
+                            <div class="col-md-6">
+                                {!! Form::input('text', 'lastname', null, ['class' => 'form-control', 'placeholder' => 'Nachname', 'required']) !!}
                             </div>
                         </div>
-                    </div>
+
+                        <div class="form-group">
+                            {!! Form::label('email', 'Email-Adresse', ['class' => 'col-md-4 control-label']) !!}
+                            <div class="col-md-6">
+                                {!! Form::input('email', 'email', null, ['class' => 'form-control', 'placeholder' => 'Email-Adresse', 'required']) !!}
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            {!! Form::label('password', 'Passwort', ['class' => 'col-md-4 control-label']) !!}
+                            <div class="col-md-6">
+                                {!! Form::input('password', 'password', null, ['class' => 'form-control', 'placeholder' => 'Leer lassen zum Deaktivieren']) !!}
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            {!! Form::label('birthday', 'Geburtstag', ['class' => 'col-md-4 control-label']) !!}
+                            <div class="col-md-6">
+                                <div class="input-group date datetimepicker" data-date-format="DD.MM.YYYY">
+                                    {!! Form::input('text', 'birthday', null, ['class' => 'form-control', 'placeholder' => 'z. B. 01.07.1985']) !!}
+                                    <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            {!! Form::label('entry_date', 'Eintrittsdatum', ['class' => 'col-md-4 control-label']) !!}
+                            <div class="col-md-6">
+                                <div class="input-group date datetimepicker" data-date-format="DD.MM.YYYY">
+                                    {!! Form::input('text', 'entry_date', null, ['class' => 'form-control', 'placeholder' => 'z. B. '.date('d.m.Y')]) !!}
+                                    <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"</span>
+                                </div>
+                            </div>
+                        </div>
 
                         @include('location_input')
 
-                    <div class="form-group">
-                        {!! Form::label('group_id', 'Gruppe', ['class' => 'col-md-4 control-label']) !!}
-                        <div class="col-md-6">
-                            {!! Form::select('group_id', $groups, null, ['class' => 'form-control']) !!}
-                        </div>
-                    </div>
-
-
-                    <div class="form-group">
-                        <div class="col-md-4"></div>
-
-                        <div class="col-md-6">
-                            <div class="checkbox">
-                                <label>
-                                    {!! Form::checkbox('active', 1, true) !!} Aktives Mitglied?
-                                </label>
+                        <div class="form-group">
+                            {!! Form::label('group_id', 'Gruppe', ['class' => 'col-md-4 control-label']) !!}
+                            <div class="col-md-6">
+                                {!! Form::select('group_id', $groups, null, ['class' => 'form-control']) !!}
                             </div>
                         </div>
-                    </div>
 
 
-                    <div class="form-group">
-                        <div class="col-md-6 col-md-offset-4">
-                            {!! Form::submit('Benutzer anlegen', ['class' => 'btn btn-primary']) !!}
+                        <div class="form-group">
+                            <div class="col-md-4"></div>
+
+                            <div class="col-md-6">
+                                <div class="checkbox">
+                                    <label>
+                                        {!! Form::checkbox('active', 1, true) !!} Aktives Mitglied?
+                                    </label>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    {!! Form::close() !!}
 
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                {!! Form::submit('Benutzer anlegen', ['class' => 'btn btn-primary']) !!}
+                            </div>
+                        </div>
+                        {!! Form::close() !!}
+
+                    </div>
                 </div>
             </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
         </div>
+
     </div>
 </div>
+
+
+
+

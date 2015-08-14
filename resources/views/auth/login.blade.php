fc6e4917aaea3a4821160a083a9dd622883e6cd2
Christian Wansart <christian@bluepanel.org>
2015-08-14 10:05:52 +0200

    translated login page. closed #28

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,11 +9,11 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Login</div>
+                    <div class="panel-heading">Anmelden</div>
                     <div class="panel-body">
                         @if (count($errors) > 0)
                             <div class="alert alert-danger">
-                                <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                                <strong>Es gab ein paar Probleme!<br><br>
                                 <ul>
                                     @foreach ($errors->all() as $error)
                                         <li>{{ $error }}</li>
@@ -26,14 +26,14 @@
                             <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
                             <div class="form-group">
-                                <label class="col-md-4 control-label">E-Mail Address</label>
+                                <label class="col-md-4 control-label">E-Mail Adresse</label>
                                 <div class="col-md-6">
                                     <input type="email" class="form-control" name="email" value="{{ old('email') }}">
                                 </div>
                             </div>
 
                             <div class="form-group">
-                                <label class="col-md-4 control-label">Password</label>
+                                <label class="col-md-4 control-label">Passwort</label>
                                 <div class="col-md-6">
                                     <input type="password" class="form-control" name="password">
                                 </div>
@@ -43,7 +43,7 @@
                                 <div class="col-md-6 col-md-offset-4">
                                     <div class="checkbox">
                                         <label>
-                                            <input type="checkbox" name="remember"> Remember Me
+                                            <input type="checkbox" name="remember"> Anmeldung merken
                                         </label>
                                     </div>
                                 </div>
@@ -51,9 +51,9 @@
 
                             <div class="form-group">
                                 <div class="col-md-6 col-md-offset-4">
-                                    <button type="submit" class="btn btn-primary">Login</button>
+                                    <button type="submit" class="btn btn-primary">Anmelden</button>
 
-                                    <a class="btn btn-link" href="{{ url('/password/email') }}">Forgot Your Password?</a>
+                                    <a class="btn btn-link" href="{{ url('/password/email') }}">Passwort vergessen?</a>
                                 </div>
                             </div>
                         </form>

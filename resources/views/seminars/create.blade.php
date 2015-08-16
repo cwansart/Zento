be9e79de13f20979f3c240016026a5936d469c08
Christian Wansart <christian@bluepanel.org>
2015-08-16 12:01:29 +0200

    extracted seminar form to partial. Closed #33

--- resources/views/seminars/create.blade.php
+++ resources/views/seminars/create.blade.php
@@ -24,24 +24,7 @@
                         @endif
 
                         {!! Form::open(array('class' => 'form-horizontal', 'method' => 'POST', 'route' => 'seminars.store')) !!}
-                        @include('location_input')
-
-                        <div class="form-group">
-                            {!! Form::label('title', 'Titel', ['class' => 'col-md-4 control-label']) !!}
-                            <div class="col-md-6">
-                                {!! Form::input('text', 'title', null, ['class' => 'form-control', 'placeholder' => 'Titel', 'required']) !!}
-                            </div>
-                        </div>
-                            
-                        <div class="form-group">
-                            {!! Form::label('date', 'Datum', ['class' => 'col-md-4 control-label']) !!}
-                            <div class="col-md-6">
-                                <div class="input-group date datetimepicker" data-date-format="DD.MM.YYYY">
-                                    {!! Form::input('text', 'date', null, ['class' => 'form-control', 'placeholder' => 'z. B. 01.07.1985']) !!}
-                                    <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span>
-                                </div>
-                            </div>
-                        </div>
+                        @include('seminars._form')
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">

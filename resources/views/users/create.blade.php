c92f23a1b35d876398f864b7ebc3c02746e6d579
Christian Wansart <christian@bluepanel.org>
2015-08-16 10:52:35 +0200

    extracted users form to partial. #33

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -24,76 +24,7 @@
                         @endif
 
                         {!! Form::open(array('class' => 'form-horizontal', 'method' => 'POST', 'route' => 'users.store')) !!}
-                        <div class="form-group">
-                            {!! Form::label('firstname', 'Vorname', ['class' => 'col-md-4 control-label']) !!}
-                            <div class="col-md-6">
-                                {!! Form::input('text', 'firstname', null, ['class' => 'form-control', 'placeholder' => 'Vorname', 'required']) !!}
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            {!! Form::label('lastname', 'Nachname', ['class' => 'col-md-4 control-label']) !!}
-                            <div class="col-md-6">
-                                {!! Form::input('text', 'lastname', null, ['class' => 'form-control', 'placeholder' => 'Nachname', 'required']) !!}
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            {!! Form::label('email', 'Email-Adresse', ['class' => 'col-md-4 control-label']) !!}
-                            <div class="col-md-6">
-                                {!! Form::input('email', 'email', null, ['class' => 'form-control', 'placeholder' => 'Email-Adresse', 'required']) !!}
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            {!! Form::label('password', 'Passwort', ['class' => 'col-md-4 control-label']) !!}
-                            <div class="col-md-6">
-                                {!! Form::input('password', 'password', null, ['class' => 'form-control', 'placeholder' => 'Leer lassen zum Deaktivieren']) !!}
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            {!! Form::label('birthday', 'Geburtstag', ['class' => 'col-md-4 control-label']) !!}
-                            <div class="col-md-6">
-                                <div class="input-group date datetimepicker" data-date-format="DD.MM.YYYY">
-                                    {!! Form::input('text', 'birthday', null, ['class' => 'form-control', 'placeholder' => 'z. B. 01.07.1985']) !!}
-                                    <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            {!! Form::label('entry_date', 'Eintrittsdatum', ['class' => 'col-md-4 control-label']) !!}
-                            <div class="col-md-6">
-                                <div class="input-group date datetimepicker" data-date-format="DD.MM.YYYY">
-                                    {!! Form::input('text', 'entry_date', null, ['class' => 'form-control', 'placeholder' => 'z. B. '.date('d.m.Y')]) !!}
-                                    <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span>
-                                </div>
-                            </div>
-                        </div>
-
-                        @include('location_input')
-
-                        <div class="form-group">
-                            {!! Form::label('group_id', 'Gruppe', ['class' => 'col-md-4 control-label']) !!}
-                            <div class="col-md-6">
-                                {!! Form::select('group_id', $groups, null, ['class' => 'form-control']) !!}
-                            </div>
-                        </div>
-
-
-                        <div class="form-group">
-                            <div class="col-md-4"></div>
-
-                            <div class="col-md-6">
-                                <div class="checkbox">
-                                    <label>
-                                        {!! Form::checkbox('active', 1, true) !!} Aktives Mitglied?
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
+                        @include('users._form')
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
@@ -112,7 +43,3 @@
 
     </div>
 </div>
-
-
-
-

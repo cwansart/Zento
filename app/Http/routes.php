cf2c2ea1984dfb1763b3d76bd85b83081a4ea040
Christian Wansart <christian@bluepanel.org>
2015-08-17 17:11:48 +0200

    removed unnecessary characters from routes file

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,23 +16,23 @@
 });*/
 
 // special routes
-Route::get('/exams/get_unreg_users/{id}', 'ExamController@getUnregisterdUsers');
-Route::get('/seminars/get_unreg_users/{id}', 'SeminarController@getUnregisterdUsers');
+Route::get('exams/get_unreg_users/{id}', 'ExamController@getUnregisterdUsers');
+Route::get('seminars/get_unreg_users/{id}', 'SeminarController@getUnregisterdUsers');
 
 // set default site
 Route::get('/', 'UserController@index');
 
-Route::get('/edit_profile', 'UserController@editProfile');
-Route::put('/edit_profile', 'UserController@updateProfile');
-Route::put('/exams/{id}/updateExam', 'ExamController@updateExam');
-Route::get('/logout', 'UserController@logout');
-Route::delete('/exams/{examid}/{userid}', 'ExamController@destroyResult');
-Route::delete('/seminars/{seminarid}/{userid}', 'SeminarController@removeUser');
+Route::get('edit_profile', 'UserController@editProfile');
+Route::put('edit_profile', 'UserController@updateProfile');
+Route::put('exams/{id}/updateExam', 'ExamController@updateExam');
+Route::get('logout', 'UserController@logout');
+Route::delete('exams/{examid}/{userid}', 'ExamController@destroyResult');
+Route::delete('seminars/{seminarid}/{userid}', 'SeminarController@removeUser');
 
 Route::resource('users', 'UserController');
 Route::resource('exams', 'ExamController');
 Route::resource('seminars', 'SeminarController');
-Route::get('/appointments/{id}', 'AppointmentController@showEvent')->where('id', '[0-9]+');
+Route::get('appointments/{id}', 'AppointmentController@showEvent')->where('id', '[0-9]+');
 Route::resource('appointments', 'AppointmentController');
 
 Route::controllers([

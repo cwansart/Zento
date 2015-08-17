b563181cf3ef3461e5e8304cd916ae81d8a040bc
Christian Wansart <christian@bluepanel.org>
2015-08-18 00:02:04 +0200

    Added fillable and fixed housenr rule in seminar model.

--- app/Seminar.php
+++ app/Seminar.php
@@ -14,13 +14,23 @@ class Seminar extends Model
     protected $dates = ['date'];
 
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'date',
+        'title',
+    ];
+
+    /**
      * Sets the validator rules.
      *
      * @var array
      */
     static public $rules = [
         'street' => 'required|min:2', // we can't use "alpha" here since some streets are abbreviated with a dot
-        'housenr' => 'required|min:2|alphanum', // since we save it as a string for occasions like "12b" alphanum's fine
+        'housenr' => 'required|min:1|alphanum', // since we save it as a string for occasions like "12b" alphanum's fine
         'zip' => 'digits:5|required', // are there any zipcodes that have more than 5 digits?
         'city' => 'min:2|alpha|required',
         'date' => 'date|required',

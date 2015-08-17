40a9ce930739240147fc571c80cc5c0befe2660e
Christian Wansart <christian@bluepanel.org>
2015-08-17 17:59:51 +0200

    Extracted find or create location code to Location model

--- app/Location.php
+++ app/Location.php
@@ -21,4 +21,36 @@ class Location extends Model
     {
         return $this->hasMany('Zento\User');
     }
+
+    public static function findOrCreate(array $location)
+    {
+        // Check if necessary keys exist, otherwise fail with 403
+        if(!array_key_exists('zip', $location) &&
+            !array_key_exists('city', $location) &&
+            !array_key_exists('street', $location) &&
+            !array_key_exists('housenr', $location) &&
+            !array_key_exists('country', $location)) {
+            abort(403, 'Unauthorized action.');
+        }
+
+        // TODO: extract this block to the Location model
+        // check if given location exists; use or create otherwise.
+        $loc = Location::where('street', '=', $location['street'])
+            ->where('housenr', '=', $location['housenr'])
+            ->where('zip', '=', $location['zip'])
+            ->where('city', '=', $location['city'])
+            ->where('country', '=', $location['country']);
+        if(!$loc->exists()) {
+            $loc = Location::create([
+                'street' => $location['street'],
+                'housenr' => $location['housenr'],
+                'zip' => $location['zip'],
+                'city' => $location['city'],
+                'country' => $location['country']
+            ]);
+        } else {
+            $loc = $loc->first();
+        }
+        return $loc;
+    }
 }

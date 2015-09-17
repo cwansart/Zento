9f8c08d8bee9adce833e9c732bef23558e32801c
ChaosBelcebub <mich3ael@web.de>
2015-09-17 21:13:09 +0200

    fix missing result in Exam.php

--- app/Exam.php
+++ app/Exam.php
@@ -32,9 +32,10 @@ class Exam extends Model
         '9. Kyu', '8. Kyu', '7. Kyu',
         '6. Kyu', '5. Kyu', '4. Kyu',
         '3. Kyu', '2. Kyu', '1. Kyu',
-        '2. Dan', '3. Dan', '4. Dan',
-        '5. Dan', '6. Dan', '7. Dan',
-        '8. Dan', '9. Dan', '10. Dan',
+        '1. Dan', '2. Dan', '3. Dan',
+        '4. Dan', '5. Dan', '6. Dan',
+        '7. Dan', '8. Dan', '9. Dan',
+        '10. Dan'
     ];
 
     /**

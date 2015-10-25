98d7f2aa8158feee4112154b90c4be8c83947de5
Christian Wansart <cwansart@users.noreply.github.com>
2015-10-25 17:43:03 +0100

    Current working copy of lists

--- resources/views/lists/_form.blade.php
+++ resources/views/lists/_form.blade.php
@@ -2,41 +2,72 @@
     <div class="row">
         {!! Form::open(array('class' => 'form-horizontal', 'method' => 'GET', 'route' => 'lists.create')) !!}
 
-        <div class="form-group">
-            {!! Form::label('firstname', 'Vorname', ['class' => 'col-md-4 control-label']) !!}
-            <label>
-                {!! Form::checkbox('firstname') !!} Sortieren: {!! Form::select('firstnameOrder', array('FIRSTNAME ASC' => 'Aufsteigend', 'FIRSTNAME DESC' => 'Absteigend'), null, ['class' => 'form-control']) !!}
-            </label>
-        </div>
+        <table class="table table-hover table-user">
+            <thead>
+            <tr>
+                <th>Vorname <a href="#"><span class="glyphicon {!! $sortBy == 'firstname:ASC' ? 'glyphicon glyphicon-sort-by-attributes-alt' : 'glyphicon glyphicon-sort-by-attributes' !!}" aria-hidden="true"></span></a></th>
+                <th>Nachname <a href="#"><span class="glyphicon {!! $sortBy == 'firstname:ASC' ? 'glyphicon glyphicon-sort-by-attributes-alt' : 'glyphicon glyphicon-sort-by-attributes' !!}" aria-hidden="true"></span></a></th>
+                <th>
+                    <a href="#" id="add-new-column"><span class="glyphicon glyphicon-plus-sign" aria-hidden="true" data-toggle="tooltip" data-placement="top" title="Spalte hinzufügen"></span></a>
 
-        <div class="form-group">
-            {!! Form::label('lastname', 'Nachname', ['class' => 'col-md-4 control-label']) !!}
-            <label>
-                {!! Form::checkbox('lastname') !!} Sortieren: {!! Form::select('lastnameOrder', array('LASTNAME ASC' => 'Aufsteigend', 'LASTNAME DESC' => 'Absteigend'), null, ['class' => 'form-control']) !!}
-            </label>
-        </div>
-
-        <div class="form-group">
-            {!! Form::label('email', 'E-Mail', ['class' => 'col-md-4 control-label']) !!}
-            <div class="col-md-6">
-                {!! Form::checkbox('email') !!}
-            </div>
-        </div>
-
-        <div class="form-group">
-            {!! Form::label('emptyColumns', 'Leerspalten', ['class' => 'col-md-4 control-label']) !!}
-            <div class="col-md-6">
-                {!! Form::input('number', 'emptyColumns') !!}
-            </div>
-        </div>
-
-        <div class="form-group">
-            <div class="col-md-6 col-md-offset-4">
-                {!! Form::submit('Liste erstellen', ['class' => 'btn btn-primary']) !!}
-            </div>
-        </div>
-
+                    <div id="add-new-column-input">
+                        <select>
+                            <option>Adresse</option>
+                            <option>Geburtsdatum</option>
+                            <option>Eintrittsdatum</option>
+                            <option>Gruppe</option>
+                        </select>
+                    </div>
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+                @foreach($users as $user)
+                    <tr>
+                        <td>{!! $user->firstname !!}</td>
+                        <td>{!! $user->lastname !!}</td>
+                        <td></td>
+                    </tr>
+                @endforeach
+                <tr>
+                    <td colspan="3" style="line-height: 20px; font-size: 48px; font-weight: bold; text-align: center">...</td>
+                </tr>
+            </tbody>
+        </table>
 
         {!! Form::close() !!}
     </div>
+
+    <style>
+        table > thead > tr > th {
+            width: 33%;
+        }
+
+        table > thead > tr > th:last-child {
+            position: relative;
+        }
+
+        table > thead > tr > th:last-child > a#add-new-column,
+        table > thead > tr > th:last-child > div#add-new-column-input {
+            position: absolute;
+            top: 7px;
+            left: 0;
+        }
+
+        table > thead > tr > th:last-child > div#add-new-column-input {
+            display: none;
+        }
+    </style>
+
+    <script>
+        $(function() {
+            // Tooltips aktivieren
+            $('[data-toggle="tooltip"]').tooltip();
+
+            $('#add-new-column').on('click', function() {
+                $(this).hide(400);
+                $(this).parent().children('div').show(400);
+            });
+        });
+    </script>
 </div>

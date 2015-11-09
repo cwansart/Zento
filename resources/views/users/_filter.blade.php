8b459ae8a235569540114ecfdebcd8d31743ba9c
Christian Wansart <cwansart@users.noreply.github.com>
2015-11-09 18:54:48 +0100

    fixed filter css, may break something else, please report!

--- resources/views/users/_filter.blade.php
+++ resources/views/users/_filter.blade.php
@@ -1,18 +1,15 @@
-<div class="container">
-    <table>
-        <tr>
-            <td>
-                {!! Form::select('a', array(-1 => 'Alle Mitglieder', 0 => 'Nur Inaktive', 1 => 'Nur Aktive'), $filterStatus, ['class' => 'form-control', 'id' => 'filterA']) !!}
-            </td>
-            <td>
-                {!! Form::select('g_id', array_merge(array(-1 => 'Alle Gruppen'), $groups), $filterGroup, ['class' => 'form-control', 'id' => 'filterG']) !!}
-            </td>
-            <td>
-                <div class="input-group">
-                    {!! Form::input('text', 's', $filterSearch, ['class' => 'form-control', 'id' => 'filterS', 'placeholder' => 'Suche...']) !!}
-                    <span class="input-group-btn"><button class="btn btn-default" id="set-filter" type="button">Los!</button></span>
-                </div>
-            </td>
-        </tr>
-    </table>
+<div class="form-inline">
+        <div class="input-group">
+            {!! Form::select('a', array(-1 => 'Alle Mitglieder', 0 => 'Nur Inaktive', 1 => 'Nur Aktive'), $filterStatus, ['class' => 'form-control', 'id' => 'filterA']) !!}
+        </div>
+
+        <div class="input-group">
+            {!! Form::select('g_id', array_merge(array(-1 => 'Alle Gruppen'), $groups), $filterGroup, ['class' => 'form-control', 'id' => 'filterG']) !!}
+        </div>
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="Search for...">
+            <span class="input-group-btn">
+                <button class="btn btn-default" type="button">Go!</button>
+            </span>
+        </div>
 </div>

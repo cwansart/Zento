50ed50ac433ee433df86e92f75c0e8bfaaffbc1f
Christian Wansart <cwansart@users.noreply.github.com>
2015-11-10 18:17:39 +0100

    tranlated string

--- resources/views/users/_filter.blade.php
+++ resources/views/users/_filter.blade.php
@@ -7,9 +7,9 @@
             {!! Form::select('g_id', array_merge(array(-1 => 'Alle Gruppen'), $groups), $filterGroup, ['class' => 'form-control', 'id' => 'filterG']) !!}
         </div>
         <div class="input-group">
-            <input type="text" class="form-control" placeholder="Search for...">
+            <input type="text" class="form-control" placeholder="Suchen nach...">
             <span class="input-group-btn">
-                <button class="btn btn-default" type="button">Go!</button>
+                <button class="btn btn-default" type="button">Suchen</button>
             </span>
         </div>
 </div>

6cf17f2e4d4bd4559ebbff34471594166e2b37c3
ChaosBelcebub <mich3ael@web.de>
2016-03-02 20:36:40 +0100

    First try accessing the log to see if my schedule worked

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,8 +2,11 @@
 
 namespace Zento\Console;
 
+use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Log;
+use Zento\Appointment;
 
 class Kernel extends ConsoleKernel
 {
@@ -24,7 +27,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('inspire')
-                 ->hourly();
+        $schedule->call(function () {
+            Log::info('Schedule started');
+            // Look for reminders
+            $appointments = Appointment::where('start', '>', Carbon::now())
+                ->where('start', '<', Carbon::now()->addHour(24));
+        })->everyMinute();
     }
 }

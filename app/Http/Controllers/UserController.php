057be31cdd99218903ce6b0a0d7ac6193ce1fedd
ChaosBelcebub <mkotzjan@users.noreply.github.com>
2016-06-13 10:13:52 +0200

    Language

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -251,7 +251,7 @@ class UserController extends Controller
         $user->update(['password' => $request['password']]);
 
         return redirect(action('UserController@index'))
-            ->with('status', 'Password für '.$user->firstname.' '.$user->lastname.' gesetzt.');
+            ->with('status', 'Passwort für '.$user->firstname.' '.$user->lastname.' gesetzt.');
     }
 
     /**
